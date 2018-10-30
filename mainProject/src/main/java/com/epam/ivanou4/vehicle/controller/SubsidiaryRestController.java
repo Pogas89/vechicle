3f47595652018d657613d88db061b83fcf27ab07
Dzmitry Ivanou4 <dzmitry_ivanou4@epam.com>
2018-10-30 10:55:01 +0300

    create Validation module

--- mainProject/src/main/java/com/epam/ivanou4/vehicle/controller/SubsidiaryRestController.java
+++ mainProject/src/main/java/com/epam/ivanou4/vehicle/controller/SubsidiaryRestController.java
@@ -1,13 +1,17 @@
 package com.epam.ivanou4.vehicle.controller;
 
+import com.epam.ivanou4.vechicle.validation.Validation;
 import com.epam.ivanou4.vehicle.model.Subsidiary;
 import com.epam.ivanou4.vehicle.service.SubsidiaryService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
 import java.net.URI;
 import java.util.List;
 
@@ -17,6 +21,7 @@ import static com.epam.ivanou4.vehicle.controller.SubsidiaryRestController.REST_
 @RequestMapping(REST_URL)
 public class SubsidiaryRestController {
     public static final String REST_URL = "/rest/subsidiary";
+    public static final String SUBSIDIARY_SCHEMA = "subsidiary_shema.json";
 
     @Autowired
     private SubsidiaryService service;
@@ -32,17 +37,24 @@ public class SubsidiaryRestController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Subsidiary> createWithLocation(@RequestBody Subsidiary subsidiary) {
-        Subsidiary createdSubsidiary = service.create(subsidiary);
-        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path(REST_URL + "/{id}")
-                .buildAndExpand(createdSubsidiary.getId()).toUri();
-        return ResponseEntity.created(uriOfNewResource).body(createdSubsidiary);
+    public ResponseEntity<Subsidiary> createWithLocation(@RequestBody String subsidiaryJson) {
+        if (new Validation().validate(subsidiaryJson, SUBSIDIARY_SCHEMA)) {
+            Subsidiary subsidiary = mapSubsidiary(subsidiaryJson);
+            Subsidiary createdSubsidiary = service.create(subsidiary);
+            URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                    .path(REST_URL + "/{id}")
+                    .buildAndExpand(createdSubsidiary.getId()).toUri();
+            return ResponseEntity.created(uriOfNewResource).body(createdSubsidiary);
+        }
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void update(@RequestBody Subsidiary subsidiary) {
-        service.update(subsidiary);
+    public void update(@RequestBody String subsidiaryJson) {
+        if (new Validation().validate(subsidiaryJson, SUBSIDIARY_SCHEMA)) {
+            Subsidiary subsidiary = mapSubsidiary(subsidiaryJson);
+            service.update(subsidiary);
+        }
     }
 
     @DeleteMapping(value = "/{id}")
@@ -53,5 +65,15 @@ public class SubsidiaryRestController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, params = "companyId")
     public List<Subsidiary> getByCompanyId(@RequestParam("companyId") String companyId) {
         return service.getByCompanyId(companyId);
+    }
+
+    private Subsidiary mapSubsidiary(String json) {
+        ObjectMapper mapper = new ObjectMapper();
+        Subsidiary subsidiary = null;
+        try {
+            subsidiary = mapper.readValue(json, Subsidiary.class);
+        } catch (IOException ignored) {
+        }
+        return subsidiary;
     }
 }
